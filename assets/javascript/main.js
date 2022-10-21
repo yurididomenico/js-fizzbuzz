0c6a9d04a8a62a3c8a89c0d52f4a2836359ca0fe
yurididomenico <yurididomenico93@gmail.com>
2022-10-21 17:56:10 +0200

    JS: Condizione multipli di 15 (FizzBuzz!)

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,6 +12,7 @@ console.log('Ciao!');
 
 let x;
 let y;
+let z;
 
 // ---------- Generatore Numeri da 1 a 100 ----------
 for(let i=1;i<=100;i++)
@@ -19,26 +20,33 @@ for(let i=1;i<=100;i++)
     //Assegno alle variabili di appoggio (x & y) il valore di i
     x = i;
     y = i;
+    z = i;
 
-    //Estrapolo il resto per i multipli di 3 e di 5
+    //Estrapolo il resto per i multipli di 3, 5 e 15
     x %= 3;
     y %= 5;
+    z %= 15;
 
-    //Interrogo le variabili appoggio (x & y):
-    //  -se il resto di x è 0 allora stampo Fizz!
+    //Interrogo le variabili appoggio (z - y - x):
+    //  -se il resto di z è 0 allora stampo FizzBuzz!
     //  -altrimenti se il resto di y è 0 stampo Buzz!
+    //  -altrimenti se il resto di x è 0 stampo Fizz!
     //  -altrimenti stampo il valore di i
-    if(x === 0)
+    if(z === 0)
     {
-        console.log("Fizz!");
+        console.log("FizzBuzz!");
     }
     else if(y === 0)
     {
         console.log("Buzz!");
     }
+    else if(x === 0)
+    {
+        console.log("Fizz!");
+    }
     else
     {
-        console.log(i)
+        console.log(i);
     }
 
 }
