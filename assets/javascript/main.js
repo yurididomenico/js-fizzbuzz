ebfb71305723d9fe00b1bcaec2d2192a41aed069
yurididomenico <yurididomenico93@gmail.com>
2022-10-21 17:47:31 +0200

    JS: Condizione multipli di 5 (Buzz!)

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,17 +11,36 @@
 console.log('Ciao!');
 
 let x;
+let y;
 
 // ---------- Generatore Numeri da 1 a 100 ----------
 for(let i=1;i<=100;i++)
 {
+    //Assegno alle variabili di appoggio (x & y) il valore di i
     x = i;
+    y = i;
+
+    //Estrapolo il resto per i multipli di 3 e di 5
     x %= 3;
+    y %= 5;
+
+    //Interrogo le variabili appoggio (x & y):
+    //  -se il resto di x è 0 allora stampo Fizz!
+    //  -altrimenti se il resto di y è 0 stampo Buzz!
+    //  -altrimenti stampo il valore di i
     if(x === 0)
-    {console.log("Fizz!");}
-    else 
-    {console.log(i);}
-    
+    {
+        console.log("Fizz!");
+    }
+    else if(y === 0)
+    {
+        console.log("Buzz!");
+    }
+    else
+    {
+        console.log(i)
+    }
+
 }
 
 
