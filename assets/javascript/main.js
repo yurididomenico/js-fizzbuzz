29d7ead0fddf2c4caafd5fe000657170706ff7ad
yurididomenico <yurididomenico93@gmail.com>
2022-10-21 17:35:46 +0200

    JS: Condizione multipli di 3 (Fizz!)

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,33 @@
 // JAVASCRIPT //
 /*
 
-Scrivi un programma che stampi in console i numeri da 1 a 100,
-ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
-Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+-Scrivi un programma che stampi in console i numeri da 1 a 100
+-Per i multipli di 3 stampi “Fizz” al posto del numero
+-Per i multipli di 5 stampi “Buzz”.
+-Per i multipli di 3 e di 5 stampi “FizzBuzz”.
 
 */
 
 console.log('Ciao!');
 
-// Generatore Numeri da 1 a 100
-for(let i=0;i<100;i++)
+let x;
+
+// ---------- Generatore Numeri da 1 a 100 ----------
+for(let i=1;i<=100;i++)
 {
-    console.log(i+1);
+    x = i;
+    x %= 3;
+    if(x === 0)
+    {console.log("Fizz!");}
+    else 
+    {console.log(i);}
+    
 }
+
+
+
+
+
 
 
 
